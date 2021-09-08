db8cf1e4c679524040db8de7ce7ae85f2b8a0d06
simingli <munique126@hotmail.com>
2021-09-08 22:19:23 +0800

    [todolist]able to check.

--- src/todoList/TodoList.js
+++ src/todoList/TodoList.js
@@ -1,18 +1,33 @@
 import React, {Component} from "react";
 
 const TodoList = ({todos, setTodos}) => {
+
     const deleteTodo = (id) => {
         const newTodos = todos.filter(todo => id !== todo.id);
         setTodos(newTodos);
     }
+
+    const changeTodoStatus = (id) => {
+        const newTodos = todos.map(todo => {
+            if (todo.id === id) {
+                return {...todo, checked: !todo.checked}
+            }
+            return todo;
+        })
+        setTodos(newTodos);
+    }
+
     return (
+
         todos.map((todo) => {
-            return <li
-                key={todo.id}
-            >
-                {todo.name}
-                <button onClick={() => deleteTodo(todo.id)}>delete</button>
-            </li>
+            return <div>
+                <li key={todo.id}
+                style={todo.checked === true ? {textDecoration: "line-through"} : {textDecoration: "none"} }>
+                    <input type="checkbox" onChange={() => changeTodoStatus(todo.id)}/>
+                    {todo.name}
+                    <button onClick={() => deleteTodo(todo.id)}>delete</button>
+                </li>
+            </div>
         })
     )
 }
